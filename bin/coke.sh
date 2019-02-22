4f810c31964819f4d54bd283862b88f09b254a51
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-02-22 17:32:58 +0900

    :sparkles: コンパイルと実行を自動化するスクリプトを作った

---
+++ bin/coke.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+var1=$1
+
+# C Compile and Execute Shell
+# chmod +x coke.shで実行権限を与える
+# ./coke.shで実行可能
+
+if [ "$var1" == "" ]; then
+    echo "No input file"
+# -f オプションを検知したら強制的にコンパイルと実行を行う
+elif [ "$var1" == "-f" ]; then
+    echo "Recognized option f"
+    echo "-------------------"
+    var2=$2
+    # name=${var2%.*}
+    cc $var2 -o ${var2%.*}.o && ./${var2%.*}.o
+# オプション無しなら.oが存在するか確認して、あるなら.oのみを実行
+elif [ -f "${var1%.*}.o" ]; then
+    echo ".o already exists"
+    echo "-----------------"
+    ./${var1%.*}.o
+# .oがないならコンパイルと実行を行う
+else
+    echo ".o does not exist"
+    echo "-----------------"
+    cc $var1 -o ${var1%.*}.o && ./${var1%.*}.o
+fi
