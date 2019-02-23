2692c29024d387af3896e3d57cc56961eb85d42a
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-02-23 18:40:04 +0900

    :sparkles: 色々な設定を自動化するスクリプトを作った
    ・fishの設定
    ・fisherのパッケージ
    ・iCloudディレクトリのシンボリックリンク作成
    ・nvimの設定

---
+++ etc/scripts/icloud.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -s $HOME/Library/Mobile\ Documents/com~apple~CloudDocs $HOME/iCloudDrive
--- etc/setup/initialize.sh
+++ etc/setup/initialize.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# ATTENTION
-# Please install iTerm2 before this shell script's execution
 
 set -eux
 
@@ -8,32 +6,4 @@ has() {
   type "$1" > /dev/null 2>&1
 }
 
-ln -s $HOME/Library/Mobile\ Documents/com~apple~CloudDocs $HOME/iCloudDrive
-has brew || brew.sh && formula.sh && cask.sh
-
-# fish setting
-mkdir -p $XDG_CONFIG_HOME/fish/functions
-cp -i src/config.fish $XDG_CONFIG_HOME/fish
-cp -i src/fisher.fish $XDG_CONFIG_HOME/fish/functions
-# curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-
-# not working below command on shell script, please execute manually
-# 3.x.x fisher add fisherman/pyenv fisherman/rbenv oh-my-fish/theme-bobthefish
-# 2.x.x fisher pyenv rbenv omf/theme-bobthefish
-# use below command yourself to set fish color skin
-# fish_config
-
-# git ignore setting
-mkdir -p $XDG_CONFIG_HOME/git
-cp -i src/ignore $XDG_CONFIG_HOME/git
-
-# neovim setting
-cp -i src/.vimrc $HOME
-mkdir -p $HOME/.vim/colors
-cp -i src/dein.toml $HOME/.vim
-cp -i src/dein_lazy.toml $HOME/.vim
-cp -i src/hybrid.vim $HOME/.vim/colors
-cp -i src/tender.vim $HOME/.vim/colors
-ln -s $HOME/.vim $XDG_CONFIG_HOME/nvim
-ln -s $HOME/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
-pip3 install neovim
+has brew && echo brew
