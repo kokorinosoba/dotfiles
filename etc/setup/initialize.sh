14f1303a28dbd6d8854bd21304afb503dbfa112f
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-02-28 10:14:53 +0900

    ヽ(ﾟ∀｡)ﾉ とりあえず、生initialize.sh

--- etc/setup/initialize.sh
+++ etc/setup/initialize.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-set -eux
+set -eu
+
+cd `dirname $0`
+cd ../scripts
+SCRIPT_DIR=$(pwd)
 
 has() {
   type "$1" > /dev/null 2>&1
 }
 
-has brew && echo brew
+echo "Continue initializing? [Y/n]"
+read ANSWER
+
+case $ANSWER in
+  "" | "Y" | "y" | "yes" | "Yes" | "YES" );;
+  * )
+    echo "Canceled."
+    exit 1;;
+esac
+
+echo $SCRIPT_DIR
+git submodule -i update
+[[ -e $HOME/iCloudDrive ]] || bash icloud.sh
+has brew || bash brew.sh
+has brew && bash formula.sh
+has brew && bash cask.sh
+has /usr/local/bin/pip3 && bash nvim.sh
+
+bash solarized.sh
+bash powerline.sh
+
+has fish && fish fish.sh
+has fish && fish fisher.sh
