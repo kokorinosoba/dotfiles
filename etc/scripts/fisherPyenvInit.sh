61bd92c9234d02906a8e390eee638a71927f9adf
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-03-15 15:12:19 +0900

    :sparkles: pyenv.fish用のパッチを作成
    'if status --is-login'とその中身を自動で削除するパッチ
    ・fishがデフォルトシェルではない場合、pyenv.fishが動かなくなる
    ・'if status --is-login'を消すことでデフォルトに設定されていなくても動くようになる

---
+++ etc/scripts/fisherPyenvInit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sed -e 's/if status --is-login//' -e '15,20 s/end//' -e '15,20 s/ *\(set -x.*\)/\1/' $HOME/.config/fish/conf.d/pyenv.fish > $TMPDIR/pyenv.fish
+cat $TMPDIR/pyenv.fish > $HOME/.config/fish/conf.d/pyenv.fish
