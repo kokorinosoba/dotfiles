8829a91d4b17eb27cd5f1a5d762140e395cbac68
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-03-17 13:58:35 +0900

    :building_construction: dotfiles/bin/* たちはコマンドと化した

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,14 +12,23 @@
 
 make -C $HOME/.config/fish/functions/Bash2FishAliasesSync sync; and source $HOME/.config/fish/b2f_aliases.fish
 
-# path to coreutil symbolic link
-set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+# path to coreutils, findutils and gnu-sed
+set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+
+set PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+set MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
+
+set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+set MANPATH /usr/local/opt/gnu-sed/libexec/gnuman $MANPATH
+
+# path to dotfiles/bin
+set PATH $HOME/dotfiles/bin $PATH
 
 # XDG Base Directory Setting
 # Use set -x (means "export") instead of export
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_CACHE_HOME $HOME/.cache
+set XDG_CONFIG_HOME $HOME/.config
+set XDG_CACHE_HOME $HOME/.cache
 
 # alias for fish
 alias relogin="exec /usr/local/bin/fish -l"
