fe18c9d5d59deb9c9f99115c2072924f5d90ba9b
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-04-23 02:47:47 +0900

    :art: 外部コマンドが存在するときのみエイリアスをつけるように変更
    ついでにexportの動作がわかってなかったので、config.fishを中心にexportするように変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,22 +13,25 @@
 make -C $HOME/.config/fish/functions/Bash2FishAliasesSync sync; and source $HOME/.config/fish/b2f_aliases.fish
 
 # path to coreutils, findutils and gnu-sed
-set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
-set PATH /usr/local/opt/findutils/libexec/gnubin $PATH
-set MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
+set -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+set -x MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
 
-set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
-set MANPATH /usr/local/opt/gnu-sed/libexec/gnuman $MANPATH
+set -x PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+set -x MANPATH /usr/local/opt/gnu-sed/libexec/gnuman $MANPATH
 
 # path to dotfiles/bin
-set PATH $HOME/dotfiles/bin $PATH
+set -x PATH $HOME/dotfiles/bin $PATH
 
 # XDG Base Directory Setting
 # Use set -x (means "export") instead of export
-set XDG_CONFIG_HOME $HOME/.config
-set XDG_CACHE_HOME $HOME/.cache
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+
+# make less avarable for colordiff
+set -x LESS -R
 
 # alias for fish
 alias relogin="exec /usr/local/bin/fish -l"
