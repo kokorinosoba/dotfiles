3147b9a5d1ee6816e9971417077aa210d2e5eb3c
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-04-23 02:41:24 +0900

    _(:3」∠)_ symlinkをオプションでunlinkするスクリプトを作った
    link.shがいらない子になったような気がするので削除予定…

--- etc/scripts/shells.sh
+++ etc/scripts/shells.sh
@@ -4,13 +4,18 @@ has() {
   type "$1" > /dev/null 2>&1
 }
 
-for shell_name in bash zsh fish
-do
-  has /usr/local/bin/$shell_name || continue
+addshell() {
+  local shell_name=$1
   if grep -E "^/usr/local/bin/$shell_name$" /etc/shells > /dev/null; then
     echo "'/usr/local/bin/$shell_name' is already in /etc/shells"
   else
     echo "'/usr/local/bin/$shell_name' is not in /etc/shells"
     echo "/usr/local/bin/$shell_name" | sudo tee -a /etc/shells
   fi
+}
+
+for shell_name in bash zsh fish
+do
+  has /usr/local/bin/$shell_name || continue
+  addshell $shell_name
 done
