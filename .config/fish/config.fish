ec8b68efd55f1e4c03641f719f459cd4369e0aec
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-10-21 18:27:58 +0900

    _(:3」∠)_ Windows用にconfig.fishを修正

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,14 +13,14 @@
 make -C $HOME/.config/fish/functions/Bash2FishAliasesSync sync; and source $HOME/.config/fish/b2f_aliases.fish
 
 # path to coreutils, findutils and gnu-sed
-set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-
-set -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
-set -x MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
-
-set -x PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
-set -x MANPATH /usr/local/opt/gnu-sed/libexec/gnuman $MANPATH
+# set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+# set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+# 
+# set -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+# set -x MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
+# 
+# set -x PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+# set -x MANPATH /usr/local/opt/gnu-sed/libexec/gnuman $MANPATH
 
 # path to dotfiles/bin
 set -x PATH $HOME/dotfiles/bin $PATH
@@ -34,10 +34,17 @@ set -x XDG_CACHE_HOME $HOME/.cache
 set -x LESS -R
 
 # alias for fish
-alias relogin="exec /usr/local/bin/fish -l"
+alias relogin="exec /usr/bin/fish -l"
 
 # display welcome message
 toilet -f future WELCOME TO FISH
 
 # display todo list
-todo ls --all
+# todo ls --all
+
+set -g theme_newline_cursor yes
+set -g theme_display_git_master_branch yes
+set -g theme_color_scheme dracula
+set -g theme_powerline_fonts no
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
