265e4cea394ce3b314b43ffa89be5f0aa967a834
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-11-22 06:18:10 +0900

    _(:3」∠)_ defaultsコマンドで設定を自動化するスクリプトを作った

---
+++ etc/init/dock.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Dockの位置を左側にする
+defaults write com.apple.dock orientation -string left
+
+# 最近使ったアプリケーションを非表示
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock recent-apps "()"
+
+# Dockのサイズを64にする
+defaults write com.apple.dock tilesize -int 64
+
+
+killall Dock
---
+++ etc/init/finder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 拡張子の表示
+defaults write -globalDomain AppleShowAllExtensions -bool true
+# 拡張子の非表示
+# defaults delete -globalDomain AppleShowAllExtensions
+
+# 検索実行時に「このMacを検索」にする
+defaults write com.apple.finder FXDefaultSearchScope -string SCev
+
+
+killall Finder
