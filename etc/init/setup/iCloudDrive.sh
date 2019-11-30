01de246c0862b6127b7acdc0abe73078296b32ed
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-12-01 06:22:34 +0900

    :recycle: unlinkが怖いのでgnu版のunlinkを使うように変更した
    unlinkはrmと実体は同じらしい

--- etc/init/setup/iCloudDrive.sh
+++ etc/init/setup/iCloudDrive.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-[[ -e $HOME/iCloudDrive ]] ||
-ln -s $HOME/Library/Mobile\ Documents/com~apple~CloudDocs $HOME/iCloudDrive
+[[ -e $HOME/iCloudDrive ]] || {
+  ln -s $HOME/Library/Mobile\ Documents/com~apple~CloudDocs $HOME/iCloudDrive &&
+  echo "Created symlink to \$HOME/iCloudDrive"
+}
