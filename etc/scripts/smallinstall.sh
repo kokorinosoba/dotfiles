0de03c827f553d49404547977b892b2550197b4f
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-12-09 10:50:07 +0900

    :bug: smallinstall.shが途中で止まるバグを修正

--- etc/scripts/smallinstall.sh
+++ etc/scripts/smallinstall.sh
@@ -4,12 +4,16 @@ set -eu
 
 readonly DOTDIR=$(cd $(dirname $0); pwd | sed -e 's/\(.*dotfiles\).*/\1/')
 readonly BINDIR=$DOTDIR/bin
+readonly SETUPDIR=$DOTDIR/etc/init/setup
 
 cd $DOTDIR
 
+[[ -d $HOME/.config ]] && echo ".config directory exists" && exit 1
+
 # install section
-$BINDIR/deploy -i
+$BINDIR/deploy
 git submodule update --init .config/fish/functions/Fisher
 git submodule update --init .config/fish/functions/Bash2FishAliasesSync
+$SETUPDIR/font-powerline.sh
 $DOTDIR/etc/init/setup/fisher-packages.sh
 $DOTDIR/etc/init/setup/fisher-pyenv.sh
