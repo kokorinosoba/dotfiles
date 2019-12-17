870093ca283462024cecedf89e8c9d098b1af2ac
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-12-18 02:01:50 +0900

    :pencil2: filter-branch.shのタイポを修正

--- etc/scripts/filter-branch.sh
+++ etc/scripts/filter-branch.sh
@@ -3,7 +3,7 @@
 git filter-branch --commit-filter '
   if [ "$GIT_AUTHOR_NAME" = "Kokorinosoba" ];
   then
-    GIT_COMMITTER_NAME="kokorinosoba>";
+    GIT_COMMITTER_NAME="kokorinosoba";
     GIT_AUTHOR_NAME="kokorinosoba";
     GIT_COMMITTER_EMAIL="34607448+kokorinosoba@users.noreply.github.com";
     GIT_AUTHOR_EMAIL="34607448+kokorinosoba@users.noreply.github.com";
