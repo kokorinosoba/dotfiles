04beedb40ba627110afc5f72c0a9f6c23da840cc
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-12-26 19:14:16 +0900

    :arrow_up: gcc-9に対応

--- etc/init/setup/c++.sh
+++ etc/init/setup/c++.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # settings required when including bits/stdc++.h
-ln -s /usr/local/bin/gcc-8 /usr/local/bin/gcc
-ln -s /usr/local/bin/g++-8 /usr/local/bin/g++
+ln -s /usr/local/bin/gcc-9 /usr/local/bin/gcc
+ln -s /usr/local/bin/g++-9 /usr/local/bin/g++
