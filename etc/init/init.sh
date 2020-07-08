c1fbec3359948fdd08b58d3e9a9bc26c6fd3e79b
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-07-08 22:15:47 +0900

    :recycle: init.shを項目ごとに実行できるように変更

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -u
 
 readonly DOTDIR=$(cd $(dirname $0); pwd | sed -e 's/\(.*dotfiles\).*/\1/')
 readonly BINDIR=$DOTDIR/bin
@@ -8,31 +8,49 @@ readonly SETUPDIR=$DOTDIR/etc/init/setup
 
 source $DOTDIR/etc/library/lib.sh
 
-ask_continue deploying
+ask_continue "deploying" && {
+  $SETUPDIR/iCloudDrive.sh
+  $SETUPDIR/vscode.sh
+  $BINDIR/deploy -f
+}
 
-$SETUPDIR/iCloudDrive.sh
-$SETUPDIR/vscode.sh
-$BINDIR/deploy -f
+ask_continue "brew initializing" && {
+  has brew || $SETUPDIR/homebrew.sh
+  has brew && $SETUPDIR/brew-packages.sh
+  has brew && $SETUPDIR/cask-packages.sh
+}
 
-ask_continue initializing
+ask_continue "shell initializing" && {
+  $SETUPDIR/loginshells.sh
+  $SETUPDIR/defaultshell.sh
+}
 
-has brew || $SETUPDIR/homebrew.sh
-has brew && $SETUPDIR/brew-packages.sh
-has brew && $SETUPDIR/cask-packages.sh
+ask_continue "atom initializing" && {
+  has apm && apm stars --install
+}
 
-$SETUPDIR/loginshells.sh
-$SETUPDIR/defaultshell.sh
+ask_continue "npm initializing" && {
+  has npm && $SETUPDIR/npm-packages.sh
+}
 
-has apm && apm stars --install
-has npm && $SETUPDIR/npm-packages.sh
-has pyenv && $SETUPDIR/pyenv.sh
-has /usr/local/bin/pip3 && $SETUPDIR/nvim.sh
-has gcc-8 && $SETUPDIR/c++.sh
+# has pyenv && $SETUPDIR/pyenv.sh
 
-$SETUPDIR/terminal-solarized.sh
-$SETUPDIR/iterm-japanesque.sh
-$SETUPDIR/font-powerline.sh
+ask_continue "nvim initializing" && {
+  has /usr/local/bin/pip3 && $SETUPDIR/nvim.sh
+}
 
-has fish && $SETUPDIR/fish.sh
-has fish && $SETUPDIR/fisher-packages.sh
-has fisher && $SETUPDIR/fisher-pyenv.sh
+ask_continue "cpp initializing" && {
+  has gcc-8 && $SETUPDIR/c++.sh
+}
+
+ask_continue "terminal initializing" && {
+  $SETUPDIR/terminal-solarized.sh
+  $SETUPDIR/iterm-japanesque.sh
+  $SETUPDIR/font-powerline.sh
+}
+
+ask_continue "fish initializing" && {
+  has fish && $SETUPDIR/fish.sh
+  has fish && $SETUPDIR/fisher-packages.sh
+  has fisher && $SETUPDIR/fisher-pyenv.sh
+}
