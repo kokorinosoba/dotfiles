86561d65ced6e8c398827d57396c2953fcde7445
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-07-08 22:34:01 +0900

    :heavy_minus_sign: brewパッケージ系のスクリプトを修正

--- etc/init/setup/brew-packages.sh
+++ etc/init/setup/brew-packages.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
 
 # brew packages install script
-packages=(bash
-          colordiff
-          coreutils
-          emacs
-          figlet
-          findutils
-          fish
-          gcc
-          ghc
-          ghostscript
-          gibo
-          git
-          gnu-sed
-          id3v2
-          imagemagick
-          mono
-          neovim
-          nkf
-          nodebrew
-          npm
-          postgresql
-          pyenv
-          pyenv-virtualenv
-          python
-          rbenv
-          rmtrash
-          toilet
-          tree
-          wget
-          zsh)
+packages=(
+  bash
+  colordiff
+  coreutils
+  emacs
+  figlet
+  findutils
+  fish
+  gcc
+  ghc
+  ghostscript
+  gibo
+  git
+  gnu-sed
+  id3v2
+  imagemagick
+  mono
+  neovim
+  nkf
+  nodebrew
+  npm
+  postgresql
+  pyenv
+  pyenv-virtualenv
+  python
+  rbenv
+  rmtrash
+  tree
+  wget
+  zsh
+)
 
 brew install ${packages[@]}
--- etc/init/setup/cask-packages.sh
+++ etc/init/setup/cask-packages.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
 # cask packages install script
-packages=(appcleaner
-          atom
-          cheatsheet
-          chromedriver
-          coteditor
-          docker
-          google-chrome
-          google-japanese-ime
-          iterm2
-          kindle
-          macdown
-          processing
-          qlmarkdown
-          shiftit
-          slack
-          texshop
-          visual-studio
-          visual-studio-code
-          wireshark
-          zotero)
+packages=(
+  appcleaner
+  atom
+  cheatsheet
+  chromedriver
+  coteditor
+  docker
+  google-chrome
+  google-japanese-ime
+  iterm2
+  kindle
+  macdown
+  qlmarkdown
+  shiftit
+  slack
+  texshop
+  visual-studio
+  visual-studio-code
+  wireshark
+  zotero
+)
 
 brew cask install ${packages[@]}
