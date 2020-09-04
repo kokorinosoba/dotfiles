2df7f19aac424d0219ec8ecb8da753b8171f37d2
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-09-05 01:32:02 +0900

    :fire: fisherの残り香を削除

--- etc/init/setup/fisher-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-# fisher packages install script
-set -l packages "fisherman/pyenv"\
-                "fisherman/rbenv"\
-                "oh-my-fish/theme-bobthefish"
-
-fisher add $packages
--- etc/init/setup/fisher-pyenv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-readonly TMPDIR=${TMPDIR%/}
-sed -e 's/if status --is-login//' -e '15,20 s/end//' -e '15,20 s/ *\(set -x.*\)/\1/' $HOME/.config/fish/conf.d/pyenv.fish > $TMPDIR/pyenv.fish
-cat $TMPDIR/pyenv.fish > $HOME/.config/fish/conf.d/pyenv.fish
