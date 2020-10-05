b1a9baf73eecf608191edd64f8e949006ab92063
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-10-06 02:07:04 +0900

    :bug: WindowsのWoLバッチでなぜかPCが起動しなかったので修正した

--- win/bin/wol.bat
+++ win/bin/wol.bat
@@ -1,11 +1,3 @@
 @echo off
-
-for /f "usebackq tokens=*" %%A in (`curl -siL -d mac=E4:54:E8:71:B6:66 -d task=send http://192.168.101.22/index.php`) do (
-  set HEAD=%%A
-  goto :exit_for
-)
-
-:exit_for
-echo %HEAD%
-
+curl -s -d dev=P20003 -d mac=E4:54:E8:71:B6:66 -d task=send http://192.168.101.22/index.php -o NUL -w %%{http_code}\n
 pause
