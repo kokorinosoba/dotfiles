8d136846abc921cf635ba6fcb1e0eb0f565d57e7
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2022-03-06 00:35:02 +0900

    :art: c++.shでシンボリックリンクを貼る対象のコマンドをバージョン11に変更

--- etc/init/setup/c++.sh
+++ etc/init/setup/c++.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # settings required when including bits/stdc++.h
-ln -s /usr/local/bin/gcc-10 /usr/local/bin/gcc
-ln -s /usr/local/bin/g++-10 /usr/local/bin/g++
+ln -s /usr/local/bin/gcc-11 /usr/local/bin/gcc
+ln -s /usr/local/bin/g++-11 /usr/local/bin/g++
