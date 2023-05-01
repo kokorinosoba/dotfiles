6b2572654fe32560389902a63215e9438558471b
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2023-05-01 12:16:53 +0900

    :art: library.shにHomebrewの実行ファイルのパスを取得する関数を追加

--- etc/library/library.sh
+++ etc/library/library.sh
@@ -4,6 +4,18 @@ has() {
   type "$1" > /dev/null 2>&1
 }
 
+get_homebrew_prefix() {
+  if has /usr/local/bin/brew; then
+    echo "/usr/local"
+  elif has /opt/homebrew/bin/brew; then
+    echo "/opt/homebrew"
+  fi
+}
+
+get_homebrew_executable() {
+  echo "$(get_homebrew_prefix)/bin/brew"
+}
+
 ask_continue() {
   local answer
   read -p "Continue $1? [Y/n]: " answer
