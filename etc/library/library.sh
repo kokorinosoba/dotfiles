d974a79d491d3f91d551ad536108d25fd26cd8da
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2023-05-01 12:42:17 +0900

    :art: dotfilesのbinやsetupディレクトリーを取得する関数を追加

--- etc/library/library.sh
+++ etc/library/library.sh
@@ -20,6 +20,22 @@ get_dotfiles_directory() {
   echo "$(cd $(dirname $0); pwd | sed -e 's/\(.*dotfiles\).*/\1/')"
 }
 
+get_dotfiles_bin_directory() {
+  echo "$(get_dotfiles_directory)/bin"
+}
+
+get_dotfiles_setup_directory() {
+  echo "$(get_dotfiles_directory)/etc/init/setup"
+}
+
+# 下記のような関数でDORFILES関連のパスの定数をexportする方法も検討したが、
+# 自動的にexportされることで挙動が不安定になる可能性を考慮して、個々の関数として定義することにした。
+# export_dotfiles_directories() {
+#   export DOTFILES_DIR="$(get_dotfiles_directory)"
+#   export DOTFILES_BIN_DIR="$(get_dotfiles_directory)/bin"
+#   export DOTFILES_SETUP_DIR="$(get_dotfiles_directory)/etc/init/setup"
+# }
+
 ask_continue() {
   local answer
   read -p "Continue $1? [Y/n]: " answer
