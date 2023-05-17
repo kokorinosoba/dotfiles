c5d0f63366eba0822683513b0d6ac75963a69323
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2023-05-17 20:37:52 +0900

    :sparkles: Hammerspoonの起動時用スクリプトを作成

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,16 @@
+hs.loadSpoon("ShiftIt")
+
+spoon.ShiftIt:bindHotkeys({
+    left = {{'alt', 'cmd'}, 'left'},
+    right = {{'alt', 'cmd'}, 'right'},
+    up = {{'alt', 'cmd'}, 'up'},
+    down = {{'alt', 'cmd'}, 'down'},
+    upleft = {{'alt', 'cmd'}, '1'},
+    upright = {{'alt', 'cmd'}, '2'},
+    botleft = {{'alt', 'cmd'}, '3'},
+    botright = {{'alt', 'cmd'}, '4'},
+    maximum = {{'alt', 'cmd'}, 'm'},
+    toggleFullScreen = {{'alt', 'cmd'}, 'f'},
+    nextScreen = {{'alt', 'cmd'}, 'n'},
+    previousScreen = {{'alt', 'cmd'}, 'p'}
+})
